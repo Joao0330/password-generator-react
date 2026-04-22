01210983eb3b57be8c524b1d7dcd8dfec1471631
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2026-04-22 17:22:11 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div>
+			<p>Password Generator P4$5W0rD! Character Length Include Uppercase Letters Include Lowercase Letters Include Numbers Include Symbols Strength Generate</p>
+		</div>
+	);
+}
+
+export default App;
